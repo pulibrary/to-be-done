9670dc5c0ef93b73fee58db609f88b04a318d0b9
Thanya Begum <tbegum@princeton.edu>
2021-07-21 10:43:13 -0400

    Show users, update homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,4 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  def create
-    @user = User.new(page_params)
-    @user.save
-  end
-
-  private
-  def page_params
-    params.require(:page).permit(:first_name, :last_name)
-  end
 end
