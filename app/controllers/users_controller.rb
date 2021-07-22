5f46b2b5f905843cf0f678ee12df412726410ff2
Thanya Begum <tbegum@princeton.edu>
2021-07-22 10:11:56 -0400

    Create user show and new views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,27 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name)
+    end
 end
