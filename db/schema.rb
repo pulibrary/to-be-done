db36b3af2ca050f5ad47da85b685e5186798ea2d
Thanya Begum <tbegum@princeton.edu>
2021-07-27 15:40:45 -0400

    Rollback database

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_145901) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
