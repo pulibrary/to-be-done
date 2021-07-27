b54ee010f260633683be7c51762af2d9b03561b3
Thanya Begum <tbegum@princeton.edu>
2021-07-27 14:25:03 -0400

    Implement last user methods, user model tests

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,8 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "users/show" do
+  let(:new_user) { User.create(:first_name => "Alina", :last_name => "Starkov") }
   before(:each) do
-    assign(:user, User.create(:first_name => "Alina", :last_name => "Starkov"))
+    assign(:user, new_user)
   end
 
   it "displays user's full name" do
@@ -11,9 +12,21 @@ RSpec.describe "users/show" do
     expect(rendered).to match /Alina Starkov/
   end
 
+  it "has link to edit" do
+    render
+
+    expect(rendered).to have_link "Edit", href: edit_user_path(new_user)
+  end
+
+  it "has link to destroy" do
+    render
+
+    expect(rendered).to have_link "Destroy", href: user_path(new_user)
+  end
+
   it "has link back to index" do
     render
 
-    expect(rendered).to have_link "Back to Users"
+    expect(rendered).to have_link "Back to Users", href: root_path
   end
 end
