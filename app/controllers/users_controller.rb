6b26dedba61f892e4b7a36c6cb675f1581636b14
Thanya Begum <tbegum@princeton.edu>
2021-07-29 11:43:55 -0400

    Add first, last name fields

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,9 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @user = current_user
   end
 
   def show
     @user = User.find(params[:id])
   end
-
-  def new
-    @user = User.new
-  end
-
-  def create
-    @user = User.create(user_params)
-
-    if @user.save
-      redirect_to @user
-    else
-      render :new
-    end
-  end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-
-    if @user.update(user_params)
-      redirect_to @user
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @user = User.find(params[:id])
-    @user.destroy
-
-    redirect_to root_path
-  end
-
-  private
-    def user_params
-      params.require(:user).permit(:first_name, :last_name)
-    end
 end
--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "home/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has link to sign up" do
+    render
+    expect(rendered).to have_link "Sign Up", href: new_user_registration_path
+  end
+
+  it "has link to log in" do
+    render
+    expect(rendered).to have_link "Log In", href: new_user_session_path
+  end
 end
