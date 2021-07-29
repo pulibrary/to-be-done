9bff20be87789201e61fef16937902c5579b43d6
Thanya Begum <tbegum@princeton.edu>
2021-07-29 10:24:46 -0400

    Customize devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root "users#index"
+  devise_for :users,
+    path: '/',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      sign_up: 'signup'
+    }
 
-  resources :users
+  authenticated :user do
+    root to: 'users#index', as: :authenticated_root
+  end
+  
+  unauthenticated :user do
+    root to: 'home#index'
+  end
 end
