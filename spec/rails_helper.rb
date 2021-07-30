dd926f9086cc4016f3edfb88e659471a6956bd02
Thanya Begum <tbegum@princeton.edu>
2021-07-30 12:06:26 -0400

    Add coverage minimum

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
 SimpleCov.start 'rails'
+SimpleCov.minimum_coverage 100
 puts "SimpleCov started successfully!"
 
 require 'spec_helper'
