bfa58340db37602daeb69bf5a17dfca78007ea60
Thanya Begum <tbegum@princeton.edu>
2021-08-05 15:58:15 -0400

    Create view spec for items#new

--- spec/views/items/index.html.erb_spec.rb
+++ spec/views/items/index.html.erb_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe "items/index" do
+RSpec.describe "items/index.html.erb", type: :view do
   pending "displays all item types"
 end
