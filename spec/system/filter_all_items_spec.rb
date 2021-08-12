b634f49f82c8b8f3b25790425a1f8366e07b9208
Thanya Begum <tbegum@princeton.edu>
2021-08-12 12:53:14 -0400

    Inline status filters shared examples

--- spec/system/filter_all_items_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-require "rails_helper"
-
-RSpec.describe "Filter all items", type: :system do
-  include_examples "filter by status", "/"
-end
--- spec/system/filter_item_type_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-require "rails_helper"
-
-RSpec.describe "Filter item type", type: :system do
-  ["/users/1/books", "/users/1/tv_shows", "/users/1/movies", "/users/1/music"].each do |url|
-    include_examples "filter by status", url
-  end
-end
